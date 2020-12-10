3bc5961146fecd41c1cb5acb921aa29c6cc04261
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2020-12-10 23:08:50 +0100

    setup.py for v1 already on pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     package_dir={'': 'src'},
-    name='better_abc',
+    name='abc_property',
     packages=find_packages(where="src"),
-    version='0.2.0',
+    version='1.0',
     description='Python ABC with a simple @abstract_property decorated checked after instantiation',
     author='Nicolas Deutschmann',
     license='MIT',
+    url='https://github.com/ndeutschmann/better_abc',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
